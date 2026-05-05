747f80b73a0bd16722bd43d9c08481e5a08207eb
V-Force Developer <developer@vforcetax.com.au>
2026-05-05 23:52:59 +1000

    Configure Firebase App Hosting secrets and clean up GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/vforce-tax-modern',
-  images: {
-    unoptimized: true,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
