a89035bc0e1f58437e14e95df24152b779bab82b
V-Force Developer <developer@vforcetax.com.au>
2026-05-05 23:34:34 +1000

    Setup GitHub Pages automated deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/vforce-tax-modern',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
